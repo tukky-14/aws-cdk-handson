2998c1126495eb7191d4e0fdc2aeb809e1fb70e1
tukky-14 <bigoonage0104@gmail.com>
2024-03-17 18:01:20 +0900

    CDKのスタックを更新

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,16 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { aws_s3 as s3 } from 'aws-cdk-lib';
 
 export class HelloCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        new s3.Bucket(this, 'MyFirstBucket', {
+            versioned: true,
+        });
+    }
 }
