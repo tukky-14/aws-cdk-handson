cc8981d7ba7fb366ac12f106c146ee8925d8a6c7
tukky-14 <bigoonage0104@gmail.com>
2024-03-17 18:12:47 +0900

    CDKのスタックを更新

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -7,6 +7,12 @@ export class HelloCdkStack extends cdk.Stack {
 
         new s3.Bucket(this, 'MyFirstBucket', {
             versioned: true,
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+            autoDeleteObjects: true,
         });
     }
 }
+
+// versioned: true：バージョニングを有効にして、バケット内のオブジェクトの各バージョンを保持します。
+// removalPolicy: cdk.RemovalPolicy.DESTROY：スタックが削除されるときに、バケットも自動的に削除されるようにします。これは開発やテスト環境で役立つ設定ですが、本番環境では慎重に使用する必要があります。
+// autoDeleteObjects: true：バケットを削除する際に、そのバケット内のオブジェクトも自動的に削除されるようにします。removalPolicy が DESTROY に設定されている場合に特に便利です。
